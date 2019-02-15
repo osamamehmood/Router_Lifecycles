2f9b164e4cb496fdd2b5e58d1aff9861d741e597
Osama <Osamamehmood786@gmail.com>
2019-02-15 14:58:14 +1000

    Create component folder and create 3 dummy components to render

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Profile from './components/Profile';
+import Home from './components/Home';
+import Posts from './components/Posts';
 
+const App = () => {
+  return(
+    <div>
+      Main Page
+      <Profile />
+      <Home />
+      <Posts />
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
