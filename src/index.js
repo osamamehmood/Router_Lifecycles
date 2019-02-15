e994bf51eee2205aa2e3cf0691da9def4ed8042a
Osama <Osamamehmood786@gmail.com>
2019-02-16 03:10:13 +1000

    Use react-router-dom to create routes for existing components

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom'
 import Profile from './components/Profile';
 import Home from './components/Home';
 import Posts from './components/Posts';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   return(
-    <div>
-      Main Page
-      <Profile />
-      <Home />
-      <Posts />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route path="/posts" component={Posts}/>
+        <Route path="/profile" component={Profile}/>
+        <Route path="/home" component={Home}/>
+      </div>
+    </BrowserRouter>
   )
 }
 
